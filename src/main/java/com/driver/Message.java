c96d741af53d7f142fb08c15be87a21ca9afda9f
Muskaan jain <muskaanjain017@gmail.com>
2023-01-29 00:02:54 +0530

    WhatsApp Solution

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,4 +7,33 @@ public class Message {
     private String content;
     private Date timestamp;
 
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date(28-01-2023);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
